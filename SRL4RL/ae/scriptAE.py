8d7f1329c5acf56b55231ae7b472a2228df8773e
astrid-merckling <astrid.merckling@gmail.com>
2021-08-29 14:47:25 +0200

    keep_seed_good

--- SRL4RL/ae/scriptAE.py
+++ SRL4RL/ae/scriptAE.py
@@ -52,19 +52,17 @@ if args.dir:
             all_dir = dir_hashCode
         else:
             all_dir = loaded_config['all_dir'] + '-' + dir_hashCode
-
-    keep_keys = list(args.__dict__.keys())
-    for k in remove_keys:
-        print(k)
-        keep_keys.remove(k)
-
-    keep_keys += ['n_stack']
-
-    select_new_args = {k: loaded_config[k] for k in keep_keys}
-    args.__dict__.update(select_new_args)    
+ 
     if args.keep_seed:
         select_new_args = {k: args.__dict__[k] for k in remove_keys}
         loaded_config.update(select_new_args)
+    else:
+        keep_keys = list(args.__dict__.keys())
+        for k in remove_keys:
+            keep_keys.remove(k)
+        keep_keys += ['n_stack']
+        select_old_args = {k: loaded_config[k] for k in keep_keys}
+        args.__dict__.update(select_old_args)           
 
     "force the Garbage Collector to release unreferenced memory"
     del select_new_args, keep_keys
